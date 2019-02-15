85fbafd413ccf20c34314216be7ef0326d77b43e
jump <jumpzaza1242@gmail.com>
2019-02-15 10:29:20 +0700

    test

--- asg_60050165.java
+++ asg_60050165.java
@@ -9,7 +9,7 @@ class asg_60050165 extends asg_2{
 	private void sky_layer1_color1(Graphics g){
 		int color=1;
 		Graphics2D g2 = (Graphics2D) g;
-		  DrawPoly(g2,new int[][] { {0,0},{50,25},{25,10} },set_CG(color));
+		  DrawPoly(g2,new int[][] { {0,0},{50,0},{25,20}},set_CG(color));
     }
 
 }
