eb387f48599685b29d20bb45a6fce67693acd97d
jump <jumpzaza1242@gmail.com>
2019-02-15 21:06:15 +0700

    update sky sun comment

--- asg_60050165.java
+++ asg_60050165.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 class asg_60050165 extends asg_2{
 	asg_60050165(Graphics g){	
-	  plot(g,set_CG(13),0,0,600,300);
+	  //plot(g,set_CG(13),0,0,600,300);
 	  sky_layer1_color1(g);
 	  sky_layer2_color2(g);
 	  sky_layer3_color2(g);
@@ -24,8 +24,8 @@ class asg_60050165 extends asg_2{
 	  sky_layer19_color10(g);
 	  sky_layer20_color11(g);
 	  sunset_color12(g);
-    }//contrater
-	private void sky_layer1_color1(Graphics g){
+    }//consutor
+   private void sky_layer1_color1(Graphics g){ //draw sky_layer1
 		int color=1;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,0},{60,0},{0,30}},set_CG(color));
@@ -36,8 +36,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {400,0},{450,0},{425,30}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {450,0},{530,0},{510,30}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {530,0},{600,0},{600,30}},set_CG(color));
-    }
-	private void sky_layer2_color2(Graphics g){
+   }//sky_layer1_color1
+   private void sky_layer2_color2(Graphics g){ //draw sky_layer2
 		int color=2;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,30},{140,30},{60,0}},set_CG(color));
@@ -47,8 +47,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {380,30},{425,30},{400,0}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {425,30},{510,30},{450,0}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {510,30},{600,30},{528,0}},set_CG(color));
-    }
-	private void sky_layer3_color2(Graphics g){
+   }//sky_layer2_color2
+   private void sky_layer3_color2(Graphics g){ //draw sky_layer3
 		int color=2;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,30},{140,30},{80,60}},set_CG(color));
@@ -58,8 +58,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {380,30},{425,30},{420,60}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {425,30},{510,30},{450,60}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {510,30},{600,30},{600,60}},set_CG(color));
-    }
-    private void sky_layer4_color3(Graphics g){
+   }//sky_layer3_color2
+   private void sky_layer4_color3(Graphics g){ //draw sky_layer4
 		int color=3;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,60},{80,60},{0,30}},set_CG(color));
@@ -69,8 +69,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {280,60},{420,60},{380,30}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {420,60},{450,60},{425,30}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {450,60},{600,60},{510,30}},set_CG(color));
-    }
-	private void sky_layer5_color3(Graphics g){
+   }//sky_layer4_color3
+   private void sky_layer5_color3(Graphics g){ //draw sky_layer5
 		int color=3;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,60},{80,60},{50,90}},set_CG(color));
@@ -80,8 +80,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {280,60},{420,60},{320,90}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {420,60},{450,60},{450,90}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {450,60},{600,60},{530,90}},set_CG(color));
-   }
-   private void sky_layer6_color4(Graphics g){
+   }//sky_layer5_color3
+   private void sky_layer6_color4(Graphics g){ //draw sky_layer6
 		int color=4;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,90},{50,90},{0,60}},set_CG(color));
@@ -92,8 +92,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {320,90},{450,90},{420,60}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {450,90},{530,90},{450,60}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {530,90},{600,90},{600,60}},set_CG(color));
-   }
-   private void sky_layer7_color4(Graphics g){
+   }//sky_layer6_color4
+   private void sky_layer7_color4(Graphics g){ //draw sky_layer7
 		int color=4;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,90},{50,90},{0,120}},set_CG(color));
@@ -104,8 +104,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {320,90},{450,90},{380,120}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {450,90},{530,90},{510,120}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {530,90},{600,90},{550,120}},set_CG(color));
-   }
-   private void sky_layer8_color5(Graphics g){
+   }//sky_layer7_color4
+   private void sky_layer8_color5(Graphics g){ //draw sky_layer8
 		int color=5;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,120},{50,120},{50,90}},set_CG(color));
@@ -116,8 +116,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {380,120},{510,120},{450,90}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {510,120},{550,120},{530,90}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {550,120},{600,120},{600,90}},set_CG(color));
-  }
-  private void sky_layer9_color5(Graphics g){
+   }//sky_layer8_color5
+   private void sky_layer9_color5(Graphics g){ //draw sky_layer9
 		int color=5;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,120},{50,120},{10,150}},set_CG(color));
@@ -128,8 +128,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {380,120},{510,120},{490,150}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {510,120},{550,120},{550,150}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {550,120},{600,120},{580,150}},set_CG(color));
-  }
-  private void sky_layer10_color6(Graphics g){
+   }//sky_layer9_color5
+   private void sky_layer10_color6(Graphics g){ //draw sky_layer10
 		int color=6;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,150},{10,150},{0,120}},set_CG(color));
@@ -141,8 +141,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {490,150},{550,150},{510,120}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {550,150},{580,150},{550,120}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {580,150},{600,150},{600,120}},set_CG(color));
-  }
-  private void sky_layer11_color6(Graphics g){
+   }//sky_layer10_color6
+   private void sky_layer11_color6(Graphics g){ //draw sky_layer11
 		int color=6;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,150},{10,150},{0,180}},set_CG(color));
@@ -154,8 +154,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {490,150},{550,150},{540,180}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {550,150},{580,150},{560,180}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {580,150},{600,150},{580,180}},set_CG(color));
-  }
-  private void sky_layer12_color7(Graphics g){
+   }//sky_layer11_color6
+   private void sky_layer12_color7(Graphics g){ //draw sky_layer12
 		int color=7;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,180},{20,180},{10,150}},set_CG(color));
@@ -167,8 +167,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {540,180},{560,180},{550,150}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {560,180},{580,180},{580,150}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {580,180},{600,180},{600,150}},set_CG(color));
-  }
-  private void sky_layer13_color7(Graphics g){
+   }//sky_layer12_color7
+   private void sky_layer13_color7(Graphics g){ //draw sky_layer13
 		int color=7;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,180},{20,180},{20,210}},set_CG(color));
@@ -180,8 +180,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {540,180},{560,180},{500,210}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {560,180},{580,180},{500,210}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {580,180},{600,180},{500,210}},set_CG(color));
-  }
-  private void sky_layer14_color8(Graphics g){
+   }//sky_layer13_color7
+   private void sky_layer14_color8(Graphics g){ //draw sky_layer14
 		int color=8;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,210},{20,210},{0,180}},set_CG(color));
@@ -191,8 +191,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {290,210},{350,210},{360,180}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {350,210},{500,210},{540,180}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {500,210},{600,210},{600,180}},set_CG(color));
-  }
-  private void sky_layer15_color8(Graphics g){
+   }//sky_layer14_color8
+   private void sky_layer15_color8(Graphics g){ //draw sky_layer15
 		int color=8;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,210},{20,210},{0,240}},set_CG(color));
@@ -202,8 +202,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {290,210},{350,210},{320,240}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {350,210},{500,210},{400,240}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {500,210},{600,210},{480,240}},set_CG(color));
-  }
-  private void sky_layer16_color9(Graphics g){
+   }// sky_layer15_color8
+   private void sky_layer16_color9(Graphics g){ //draw sky_layer16
 		int color=9;
 		Graphics2D g2 = (Graphics2D) g;
 		   DrawPoly(g2,new int[][] { {0,240},{50,240},{21,210}},set_CG(color));
@@ -213,8 +213,8 @@ class asg_60050165 extends asg_2{
 		   DrawPoly(g2,new int[][] { {320,240},{400,240},{350,210}},set_CG(color));
 		   DrawPoly(g2,new int[][] { {400,240},{480,240},{500,210}},set_CG(color));
 		   DrawPoly(g2,new int[][] { {480,240},{600,240},{600,210}},set_CG(color));
-  }
-  private void sky_layer17_color9(Graphics g){
+   }//sky_layer16_color9
+   private void sky_layer17_color9(Graphics g){ //draw sky_layer17
 		int color=9;
 		Graphics2D g2 = (Graphics2D) g;
 		   DrawPoly(g2,new int[][] { {0,240},{50,240},{40,270}},set_CG(color));
@@ -224,8 +224,8 @@ class asg_60050165 extends asg_2{
 		   DrawPoly(g2,new int[][] { {320,240},{400,240},{350,270}},set_CG(color));
 		   DrawPoly(g2,new int[][] { {400,240},{480,240},{400,270}},set_CG(color));
 		   DrawPoly(g2,new int[][] { {480,240},{600,240},{600,270}},set_CG(color));
-  }
-  private void sky_layer18_color10(Graphics g){
+   }//sky_layer17_color9
+   private void sky_layer18_color10(Graphics g){ //draw sky_layer18
 		int color=10;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,270},{40,270},{0,240}},set_CG(color));
@@ -233,8 +233,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {150,270},{350,270},{300,240}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {350,270},{400,270},{400,240}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {400,270},{600,270},{480,240}},set_CG(color));
-  }
-  private void sky_layer19_color10(Graphics g){
+   }//sky_layer18_color10
+   private void sky_layer19_color10(Graphics g){ //draw sky_layer19
 		int color=10;
 		Graphics2D g2 = (Graphics2D) g;
 		  DrawPoly(g2,new int[][] { {0,270},{40,270},{0,300}},set_CG(color));
@@ -242,8 +242,8 @@ class asg_60050165 extends asg_2{
 		  DrawPoly(g2,new int[][] { {150,270},{350,270},{300,300}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {350,270},{400,270},{400,300}},set_CG(color));
 		  DrawPoly(g2,new int[][] { {400,270},{600,270},{480,300}},set_CG(color));
-  }
-  private void sky_layer20_color11(Graphics g){
+   }//sky_layer19_color10
+   private void sky_layer20_color11(Graphics g){ //draw sky_layer20
 		int color=11;
 		Graphics2D g2 = (Graphics2D) g;
 		   DrawPoly(g2,new int[][] { {0,300},{50,300},{40,270}},set_CG(color));
@@ -251,12 +251,10 @@ class asg_60050165 extends asg_2{
 		   DrawPoly(g2,new int[][] { {300,300},{400,300},{350,270}},set_CG(color));
 		   DrawPoly(g2,new int[][] { {400,300},{490,300},{400,270}},set_CG(color));
 		   DrawPoly(g2,new int[][] { {484,300},{600,300},{600,270}},set_CG(color));
-  }
-  private void sunset_color12(Graphics g){
+   }//sky_layer20_color11
+   private void sunset_color12(Graphics g){ //draw sunset
 	    int color=12;
 		Graphics2D g2 = (Graphics2D) g;
 		  midpointCircle(g, g2, addP(265,265),30,set_CG(color));
-  }
-
-
-}
+   }//sunset_color12
+}//class
